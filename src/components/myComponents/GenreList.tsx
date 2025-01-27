b39e920cba9549d15e18806dcd7c8cb6ec115456
Ameer372 <ameersalim80@gmail.com>
2025-01-27 15:25:21 +0300

    create genre skeleton

--- src/components/myComponents/GenreList.tsx
+++ src/components/myComponents/GenreList.tsx
@@ -1,9 +1,25 @@
 import useGenres from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, Image, List, Text } from "@chakra-ui/react";
+import { HStack, Icon, Image, List, Text } from "@chakra-ui/react";
+import { FaExclamationTriangle } from "react-icons/fa";
+import GenreListSkeleton from "./GenreListSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) {
+    return (
+      <Text textAlign={"center"} fontSize={"lg"} color={"red.500"}>
+        <Icon as={FaExclamationTriangle}></Icon> something went wrong: ( cannot
+        load side panel ).
+      </Text>
+    );
+  }
+
+  if (isLoading) {
+    return <GenreListSkeleton />;
+  }
+
   return (
     <List.Root>
       {data.map((genre) => (
---
+++ src/components/myComponents/GenreListSkeleton.tsx
@@ -0,0 +1,10 @@
+import { SkeletonText } from "../ui/skeleton";
+const GenreListSkeleton = () => {
+  return (
+    <>
+      <SkeletonText noOfLines={20} gap={4} />
+    </>
+  );
+};
+
+export default GenreListSkeleton;
