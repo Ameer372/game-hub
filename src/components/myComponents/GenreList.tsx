af771e0ea5eb859b65ca71ef5f169960b0552203
Ameer372 <ameersalim80@gmail.com>
2025-02-11 11:30:07 +0300

    clean up genres

--- src/components/myComponents/GenreList.tsx
+++ src/components/myComponents/GenreList.tsx
@@ -1,6 +1,14 @@
 import useGenres, { Genre } from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, Icon, Image, Link, List, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  HStack,
+  Icon,
+  Image,
+  Link,
+  List,
+  Text,
+} from "@chakra-ui/react";
 import { FaExclamationTriangle } from "react-icons/fa";
 import GenreListSkeleton from "./GenreListSkeleton";
 
@@ -24,32 +32,40 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
   }
 
   return (
-    <List.Root>
-      {data
-        .filter(
-          (genre) => !["Shooter", "Sports", "Fighting"].includes(genre.name)
-        )
-        .map((genre) => (
-          <List.Item key={genre.id} as={"ul"} paddingY={1}>
-            <HStack>
-              <Image
-                src={getCroppedImageUrl(genre.image_background)}
-                alt={genre.name}
-                boxSize="32px"
-                borderRadius={8}
-                objectFit="cover"
-              />
-              <Link
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-                variant={"underline"}
-                onClick={() => onSelectGenre(genre)}
-              >
-                {genre.name}
-              </Link>
-            </HStack>
-          </List.Item>
-        ))}
-    </List.Root>
+    <>
+      <Heading fontSize="2xl" mb={3}>
+        Genres
+      </Heading>
+
+      <List.Root>
+        {data
+          .filter(
+            (genre) => !["Shooter", "Sports", "Fighting"].includes(genre.name)
+          )
+          .map((genre) => (
+            <List.Item key={genre.id} as={"ul"} paddingY={1}>
+              <HStack>
+                <Image
+                  src={getCroppedImageUrl(genre.image_background)}
+                  alt={genre.name}
+                  boxSize="32px"
+                  borderRadius={8}
+                  objectFit="cover"
+                />
+                <Link
+                  fontWeight={
+                    genre.id === selectedGenre?.id ? "bold" : "normal"
+                  }
+                  variant={"underline"}
+                  onClick={() => onSelectGenre(genre)}
+                >
+                  {genre.name}
+                </Link>
+              </HStack>
+            </List.Item>
+          ))}
+      </List.Root>
+    </>
   );
 };
 
