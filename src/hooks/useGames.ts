35e05124f38d1fdd8443a46c3f251e3b4f4ec960
Ameer Salim <ameersalim80@gmail.com>
2025-03-03 22:49:58 +0300

    Refactor/ Simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import { GameQuery } from "@/App";
+import ms from 'ms'
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { Platform } from "./usePlatforms";
 import APIClient, {FetchResponse} from "@/services/api-client";
@@ -26,7 +27,7 @@ const useGames = (gameQuery: GameQuery) =>
         page: pageParam,
     },
     }),
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms('24h'),
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ?  allPages.length + 1 : undefined; 
     }
