bedebb5ad7f722b9f0d49d3e5e9b37d2189a4aea
Ameer Salim <ameersalim80@gmail.com>
2025-03-12 21:09:12 +0300

    Refactor/ Move entity interfaces

---
+++ src/entities/Game.ts
@@ -0,0 +1,11 @@
+import { Platform } from "@/entities/Platform";
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  description_raw: string;
+  slug: string;
+}
