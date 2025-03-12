79535779bbc1775cf0bfd362474c71f867b944bf
Ameer Salim <ameersalim80@gmail.com>
2025-03-12 19:33:28 +0300

    Setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { Provider } from "@/components/ui/provider.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 const queryClient = new QueryClient();
 
@@ -12,8 +13,8 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
       <QueryClientProvider client={queryClient}>
-        <App />
-        <ReactQueryDevtools/>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </Provider>
   </StrictMode>
